c9e41633d7a3ac6ae067b5837d3b2d1c7fd6cbec
prixus <prixus@github.com>
2021-05-15 18:55:38 +0800

    Integrated User management models

--- backend/backend/todo/models.py
+++ backend/backend/todo/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+
 class Todo(models.Model):
+    class Meta:
+        managed = False
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
