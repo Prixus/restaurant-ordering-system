2f94bd3b96ab8c321d368479231c1ff45783f1e5
prixus <prixus@github.com>
2021-06-08 21:18:48 +0800

    Setup for frontend

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,189 +1,113 @@
 import React, { Component } from 'react';
-import Modal from './components/Modal';
-import axios from 'axios'
-// const todoItems = [
-//   {
-//     id: 1,
-//     title: 'Go to Market',
-//     description: 'Buy ingredients to prepare dinner',
-//     completed: true,
-//   },
-//   {
-//     id: 2,
-//     title: 'Study',
-//     description: 'Read Algebra and History textbook for the upcoming test',
-//     completed: false,
-//   },
-//   {
-//     id: 3,
-//     title: 'Sammy\'s books',
-//     description: 'Go to library to return Sammy\'s books',
-//     completed: true,
-//   },
-//   {
-//     id: 4,
-//     title: 'Article',
-//     description: 'Write article on how to use Django with React',
-//     completed: false,
-//   },
-// ]
+import Nav from './components/Nav';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+import './App.css';
 
 class App extends Component {
-  state = {
-    viewCompleted: false,
-    todoList: [],
-    modal: false,
-    activeItem: {
-      title: '',
-      description: '',
-      completed: false,
-    },
-  };
-
-  async componentDidMount() {
-    await this.refreshList();
-  };
-
-  refreshList = async () => {
-    try {
-      const { data } = await axios.get('/api/todos/');
-      this.setState({todoList : data})
-    } catch (Exception) {
-      console.log(Exception)
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayed_form: '',
+      logged_in: !!localStorage.getItem('token'),
+      username: ''
     }
-  };
+  }
 
-  toggle = () => {
-    this.setState({ modal: !this.state.modal })
-  };
-
-  handleSubmit = async (item) => {
-    this.toggle();
-    try {
-      if (item.id) {
-        await axios.put(`/api/todos/${item.id}/`, item);
-        return
-      }
-      await axios.post('/api/todos/', item)
+  componentDidMount() {
+    if (this.state.logged_in) {
+      console.log(localStorage.getItem('token'));
+      fetch('http://localhost:8000/api/current_user/', {
+        headers : {
+          Authorization: `JWT ${localStorage.getItem('token')}`
+        }
+      }).then(res => res.json())
+        .then(json => {
+          this.setState({username: json.username})
+        })
     }
-    catch(Exception) {
-      console.log(Exception);
-   } finally {
-    this.refreshList()
-   }
+  }
+
+  handle_login = (e, data) => {
+    e.preventDefault();
+    fetch('http://localhost:8000/api/token-auth/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(res => res.json())
+      .then(json => {
+          localStorage.setItem('token', json.token);
+          this.setState({
+            logged_in: true,
+            displayed_form: '',
+            username: json.username
+          });
+        });
   };
 
-  handleDelete = async (item) => {
-    await axios.delete(`/api/todos/${item.id}`);
-    this.refreshList()
+  handle_signup = (e, data) => {
+    e.preventDefault();
+    fetch('http://localhost:8000/api/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(res => res.json())
+        .then(json => {
+          console.log(json, data);
+          localStorage.setItem('token', json.token);
+          this.setState({
+            logged_in: true,
+            displayed_form: '',
+            username: json.username
+          });
+        });
   };
 
-  createItem = () => {
-    const item = { title: '', description: '', completed: false };
-
-    this.setState({ activeItem: item, modal: !this.state.modal })
+  handle_logout = () => {
+    localStorage.removeItem('token');
+    this.setState({ logged_in: false, username: ''});
   };
 
-  editItem = (item) => {
-    this.setState({ activeItem: item, modal: !this.state.modal })
-  };
-
-  displayCompleted = (status) => {
-    if (status) {
-      return this.setState({ viewCompleted: true })
-    }
-
-    return this.setState({ viewCompleted: false })
-  };
-
-  renderTabList = () => {
-    return (
-      <div className='nav nav-tabs'>
-        <span
-          className={this.state.viewCompleted ? 'nav-link active' : 'nav-link'}
-          onClick={() => this.displayCompleted(true)}
-        >
-          Complete
-        </span>
-        <span
-          className={this.state.viewCompleted ? 'nav-link' : 'nav-link active'}
-          onClick={() => this.displayCompleted(false)}
-        >
-          Incomplete
-        </span>
-      </div>
-    )
-  };
-
-  renderItems = () => {
-    const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
-      (item) => item.completed === viewCompleted
-    );
-
-    return newItems.map((item) => (
-      <li
-        key={item.id}
-        className='list-group-item d-flex justify-content-between align-items-center'
-      >
-        <span
-          className={`todo-title mr-2 ${
-            this.state.viewCompleted ? 'completed-todo' : ''
-          }`}
-          title={item.description}
-        >
-          {item.title}
-        </span>
-        <span>
-          <button
-            className='btn btn-secondary mr-2'
-            onClick={() => this.editItem(item)}
-          >
-            Edit
-          </button>
-          <button
-            className='btn btn-danger'
-            onClick={() => this.handleDelete(item)}
-          >
-            Delete
-          </button>
-        </span>
-      </li>
-    ))
+  display_form = form => {
+    this.setState({
+      displayed_form: form
+    });
   };
 
   render() {
+    let form;
+    switch (this.state.displayed_form) {
+      case 'login':
+        form = <LoginForm handle_login={this.handle_login} />;
+        break;
+      case 'signup':
+        form = <SignupForm handle_signup={this.handle_signup} />;
+        break;
+      default:
+        form = null;
+    }
+
     return (
-      <main className='container'>
-        <h1 className='text-white text-uppercase text-center my-4'>Todo app</h1>
-        <div className='row'>
-          <div className='col-md-6 col-sm-10 mx-auto p-0'>
-            <div className='card p-3'>
-              <div className='mb-4'>
-                <button
-                  className='btn btn-primary'
-                  onClick={this.createItem}
-                >
-                  Add task
-                </button>
-              </div>
-              {this.renderTabList()}
-              <ul className='list-group list-group-flush border-top-0'>
-                {this.renderItems()}
-              </ul>
-            </div>
-          </div>
-        </div>
-        {this.state.modal ? (
-          <Modal
-            activeItem={this.state.activeItem}
-            toggle={this.toggle}
-            onSave={this.handleSubmit}
+        <div className="App">
+          <Nav
+              logged_in={this.state.logged_in}
+              display_form={this.display_form}
+              handle_logout={this.handle_logout}
           />
-        ) : null}
-      </main>
+          {form}
+          <h3>
+            {this.state.logged_in
+                ? `Hello, ${this.state.username}`
+                : 'Please Log in'
+            }
+          </h3>
+        </div>
     )
   }
 }
 
-export default App
+export default App;
